c6022f498d046432ace97e46b405434bccc246a1
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2019-03-24 14:57:47 -0600

    added appbar

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './Resources/styles.css';
-
+import Header from './components/header_footer/Header.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header/>
           <p>
-            Hello world
+          hello
           </p>
 
       </div>
---
+++ landingpage/src/components/header_footer/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+class header extends Component{
+  render(){
+    return(
+      <div>
+      header
+      </div>
+    );
+  }
+}
+
+export default header;
